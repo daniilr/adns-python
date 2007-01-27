690e8c3792252327750ebe979044c6b46cc7eb3a
farcepest <farcepest@37c91ceb-9123-0410-b5bc-c5f3480007e7>
2007-01-27 19:26:47 +0000

    Add ChangeLog. Version 1.2.1

--- setup.py
+++ setup.py
@@ -29,12 +29,12 @@ http://www.gnu.org/software/adns/
 """
 setup (# Distribution meta-data
     name = "adns-python",
-    version = "1.2.0",
+    version = "1.2.1",
     description = "An interface to GNU adns",
     author = "Andy Dustman",
     author_email = "farcepest@gmail.com",
     url = "http://code.google.com/p/adns-python",
-    download_url = "http://adns-python.googlecode.com/files/adns-python-1.2.0.tar.gz",
+    download_url = "http://adns-python.googlecode.com/files/adns-python-1.2.1.tar.gz",
     long_description=long_description,
     license = "GPL",
     classifiers = [
